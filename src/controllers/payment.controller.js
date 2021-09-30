1ed8d955b21ed911c2d22d8fa3678d31b486906d
Ramendra <ramendra_fw10_090@masai.school>
2021-09-30 22:45:03 +0530

    Linking completion pending

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const Payment = require("../models/payment.model");
+const Course = require("../models/course.model")
 
 const router = express.Router();
 
@@ -8,8 +9,10 @@ router.get("/payment", async (req, res) => {
     const payment = await Payment.find();
     return res.status(200).send({ payment });
 })
-router.get("/course/:id/payment", async (req, res) => {
-    const payment = await Payment.findById(req.params.id);
+
+
+router.get("/payment/:id", async (req, res) => {
+    const payment = await Course.findById(req.params.id);
     return res.render("payment.ejs", { payment });
 })
 
@@ -19,14 +22,14 @@ router.get("/course/:id/payment", async (req, res) => {
 //creating api for index page view
 router.get("/payment/get", async (req, res) => {
     const payment = await payment.find().lean().exec();
-    return res.render('../views/payment.ejs',{
-        payment:payment
+    return res.render('../views/payment.ejs', {
+        payment: payment
     })
 })
 
 router.post("/payment", async (req, res) => {
     const payment = await Payment.create(req.body);
-   return res.status(201).send({ payment })
-    });
+    return res.status(201).send({ payment })
+});
 
 module.exports = router
