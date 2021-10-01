70ad065b8c999426657f68bb20bf615f5ffc9182
Pavankalyan477 <sarakantipavankalyan070@gmail.com>
2021-10-01 15:35:45 +0530

    udacity

--- src/models/signup.model.js
+++ src/models/signup.model.js
@@ -7,7 +7,8 @@ const signupSchema = new mongoose.Schema({
     "First_Name": { type: String, required: true },
     "Last_Name": { type: String, required: true },
     "Email_Address": { type: String, required: true },
-    "Password": { type: String, required: true }
+    "Password": { type: String, required: true },
+    "course":[{type:mongoose.Schema.Types.ObjectId,required:true}]
 })
 
 //signupSchema.plugin(passportLocalMongoose);
